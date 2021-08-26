5acb0c1a7ae381de08db25a83caec6777b904001
chanhminh99 <chungminhchanh1999@gmail.com>
2021-08-27 00:07:23 +0700

    Alternatively way with immer produce: curried producer that immer wrap the recipe func and return curried

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -7,34 +7,25 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham Chung', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
-    return produce(state, (draftState) => {
-      const item = {uuid: id++, quantity: 1, ...action.payload}
-      draftState.push(item)
-    })
+    const item = {uuid: id++, quantity: 1, ...action.payload}
+    state.push(item)
   }
 
   if (action.type === ITEM_REMOVED) {
-    const newItemList = state.filter((item) => item.uuid !== action.payload)
-    return newItemList
+    return state.filter((item) => item.uuid !== action.payload)
   }
 
   if (action.type === ITEM_PRICE_UPDATED) {
-    return produce(state, (draftState) => {
-      const item = draftState.find((item) => item.uuid === action.payload.uuid)
-      item.price = parseInt(action.payload.price, 10)
-    })
+    const item = state.find((item) => item.uuid === action.payload.uuid)
+    item.price = parseInt(action.payload.price, 10)
   }
 
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return produce(state, (draftState) => {
-      const item = draftState.find((item) => item.uuid === action.payload.uuid)
-      item.quantity = parseInt(action.payload.quantity, 10)
-    })
+    const item = state.find((item) => item.uuid === action.payload.uuid)
+    item.quantity = parseInt(action.payload.quantity, 10)
   }
-
-  return state;
-};
+}, initialItems);
 
 export default reducer;
